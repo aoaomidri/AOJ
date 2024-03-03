a976c4aa11b6cecf7c5c2bc3ef1b5d6dbd596c0b
aoaomidri <aoaomidori2723@gmail.com>
2024-03-03 11:07:14 +0900

    clear

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
 
 int main() {
+	std::string s, p;
+	
+	bool isSucces = false;	
+
+	cin >> s >> p;	
+
+	s = s + s;
+
+	if (s.find(p)!=string::npos){
+		isSucces = true;
+	}
+		
+	if (isSucces){
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
 
 	return 0;
 }
