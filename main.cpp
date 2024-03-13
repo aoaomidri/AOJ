aeb7627ef63faefd0ff6eb2977e65dab75a90c7c
aoaomidri <aoaomidori2723@gmail.com>
2024-03-13 17:21:52 +0900

    complete

--- main.cpp
+++ main.cpp
@@ -4,26 +4,41 @@
 using namespace std;
 
 int main() {
-	string s, p;
-	int hanakoP = 0, taroP = 0;
+	string base, order;
 	int count = 0, i = 0;
+	int a = 0, b = 0;
+	cin >> base;
 	cin >> count;
 	while (i < count) {
-		cin >> s >> p;
-		if (s > p) {
-			taroP += 3;
-		}
-		else if (s < p) {
-			hanakoP += 3;
-		}
-		else if (s == p) {
-			taroP++;
-			hanakoP++;
-		}
+		cin >> order;
+		if (order == "replace"){
+			string change;
+			cin >> a >> b >> change;
+			for (int i = a; i <= b; ++i) {
+				base[i] = change[static_cast<std::basic_string<char, std::char_traits<char>, std::allocator<char>>::size_type>(i) - a];
+			}
 
+		}
+		else if (order == "reverse") {
+			cin >> a >> b;
+			while (a < b) {
+				swap(base[a], base[b]);
+				++a;
+				--b;
+			}
+			
+		}
+		else if (order == "print") {
+			cin >> a >> b;
+			for (int i = a; i <= b; ++i) {
+				cout << base[i];
+			}
+			cout << endl;
+		}
+		
 		i++;
 	}
-	cout << taroP << ' ' << hanakoP << endl;
+	
 
 	return 0;
 }
