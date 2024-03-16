dc0ec1d6f74f7b8b9e79133781431dc78803966c
aoaomidri <aoaomidori2723@gmail.com>
2024-03-16 21:34:40 +0900

    complete

--- main.cpp
+++ main.cpp
@@ -1,44 +1,18 @@
 #include<iostream>
-#include<string>
+#include<math.h>
 
 using namespace std;
 
 int main() {
-	string base, order;
-	int count = 0, i = 0;
-	int a = 0, b = 0;
-	cin >> base;
-	cin >> count;
-	while (i < count) {
-		cin >> order;
-		if (order == "replace"){
-			string change;
-			cin >> a >> b >> change;
-			for (int i = a; i <= b; ++i) {
-				base[i] = change[static_cast<std::basic_string<char, std::char_traits<char>, std::allocator<char>>::size_type>(i) - a];
-			}
+	double x1 = 0, y1 = 0;
+	double x2 = 0, y2 = 0;
 
-		}
-		else if (order == "reverse") {
-			cin >> a >> b;
-			while (a < b) {
-				swap(base[a], base[b]);
-				++a;
-				--b;
-			}
-			
-		}
-		else if (order == "print") {
-			cin >> a >> b;
-			for (int i = a; i <= b; ++i) {
-				cout << base[i];
-			}
-			cout << endl;
-		}
-		
-		i++;
-	}
-	
+	double result = 0.0f;
 
+	cin >> x1 >> y1 >> x2 >> y2;
+
+	result = static_cast<double>(sqrt((pow((x2 - x1), 2.0f) + pow((y2 - y1), 2.0f))));
+
+	cout << std::fixed << result << endl;
 	return 0;
 }
